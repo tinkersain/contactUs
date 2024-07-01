a9056568e7099f035ef755fdc0180c18d5aaf1d4
tinkersain <tanishakar04@gmail.com>
2024-07-02 01:59:30 +0530

    form completed

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const button11 = document.getElementById("close-btn");
-const parent = document.getElementById("parent");
+// const button11 = document.getElementById("close-btn");
+// const parent = document.getElementById("parent");
 
-button11.addEventListener("click", () => {
-  if (parent.classList.contains("hide-class")) {
-    parent.classList.remove("hide-class");
-  } else {
-    parent.classList.add("hide-class");
-  }
-});
+// button11.addEventListener("click", () => {
+//   if (parent.classList.contains("hide-class")) {
+//     parent.classList.remove("hide-class");
+//   } else {
+//     parent.classList.add("hide-class");
+//   }
+// });
 
-const inputt = document.getElementById("text-inp");
+// const inputt = document.getElementById("text-inp");
+// const button = document.getElementById("submit-btn");
+
+// button.addEventListener("click", () => {
+//   const name = inputt.value;
+//   console.log(name);
+//   inputt.value = "";
+// });
+const input1 = document.getElementById("name");
+const input2 = document.getElementById("suggested");
 const button = document.getElementById("submit-btn");
-
 button.addEventListener("click", () => {
-  const name = inputt.value;
-  console.log(name);
-  inputt.value = "";
+  const name = input1.value;
+  const suggested = input2.value;
+  console.log(name, suggested);
 });
