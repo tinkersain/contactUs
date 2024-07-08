2544b164d912f674a5f92aaea1f848872449aeec
addymistrel <149488588+tinkersain@users.noreply.github.com>
2024-07-08 11:51:12 +0530

    dynamic insertion added

--- imgdesc.js
+++ imgdesc.js
@@ -10,3 +10,43 @@ function handleClick() {
     document.getElementById("data2").textContent = input2;
   }
 }
+
+/* For Dynamic insertion */
+
+/* let state = true;
+function addInParentContent(state, input1, input2) {
+  const content1 = `<div class="content1">
+          <div class="data" id="data1">${input1}</div>
+          <div class="image" id="image1">
+            <img
+              src=${input2}
+              alt="Book Cover 1"
+            />
+          </div>
+        </div>`;
+
+  const content2 = `<div class="content2">
+          <div class="image" id="image2">
+            <img
+              src=${input2}
+              alt="Book Cover 2"
+            />
+          </div>
+          <div class="data" id="data2">${input1}</div>
+        </div>`;
+
+  const parentContent = document.getElementById("parentContent");
+  if (state) parentContent.innerHTML += content1;
+  else parentContent.innerHTML += content2;
+}
+
+function handleClick() {
+  const input1 = document.getElementById("input1").value;
+  const input2 = document.getElementById("input2").value;
+  if (input1.length === 0 || input2.length === 0) {
+    alert("Please fill both the fields !");
+  } else {
+    addInParentContent(state, input1, input2);
+    state = !state;
+  }
+} */
